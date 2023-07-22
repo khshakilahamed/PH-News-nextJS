ba5aeb490bdc89085ae0b1fe06f6ac0579f592ae
khshakilahamed <khshakil.ahamed18@gmail.com>
2023-07-22 15:59:13 +0600

    Done

---
+++ src/pages/news/[newsId].js
@@ -0,0 +1,114 @@
+import RootLayout from "@/components/Layouts/RootLayout";
+import {
+  CalendarOutlined,
+  CommentOutlined,
+  ProfileOutlined,
+} from "@ant-design/icons";
+import { Col, Row } from "antd";
+import Image from "next/image";
+import React from "react";
+
+const NewsDetailPage = ({ news }) => {
+  if (!news) {
+    return <p>Loading....</p>;
+  }
+  return (
+    <div style={{ margin: "40px" }}>
+      <Row
+        gutter={{
+          xs: 8,
+          sm: 16,
+          md: 24,
+          lg: 32,
+        }}
+      >
+        <Col className="gutter-row" span={12}>
+          <div>
+            <Image
+              alt="news image"
+              src={news?.image_url}
+              width={500}
+              height={300}
+              responsive
+            />
+          </div>
+        </Col>
+        <Col className="gutter-row" span={12}>
+          <div>
+            <h1 style={{ fontSize: "25px" }}>{news?.title}</h1>
+            <div
+              className="line"
+              style={{
+                height: "5px",
+                margin: "20px 0",
+                background: "#000",
+                width: "100%",
+              }}
+            ></div>
+
+            <p
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "100%",
+                color: "gray",
+                margin: "10px 0px",
+                fontSize: "12px",
+              }}
+            >
+              <span>
+                <CalendarOutlined /> {news?.release_date}
+              </span>
+              <span>
+                <CommentOutlined /> {news?.comment_count} COMMENTS
+              </span>
+              <span>
+                <ProfileOutlined /> {news?.category}
+              </span>
+            </p>
+            <p style={{ fontSize: "20px" }}> {news?.description}</p>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default NewsDetailPage;
+
+NewsDetailPage.getLayout = function getLayout(page) {
+  return <RootLayout>{page}</RootLayout>;
+};
+
+export const getServerSideProps = async (context) => {
+  const { params } = context;
+  const res = await fetch(`http://localhost:5000/news/${params.newsId}`);
+  const data = await res.json();
+  return {
+    props: {
+      news: data,
+    },
+  };
+};
+
+// export const getStaticPaths = async () => {
+//   const res = await fetch("http://localhost:5000/news");
+//   const newses = await res.json();
+
+//   const paths = newses.map((news) => ({
+//     params: { newsId: news.id },
+//   }));
+
+//   return { paths, fallback: true };
+// };
+
+// export const getStaticProps = async (context) => {
+//   const { params } = context;
+//   const res = await fetch(`http://localhost:5000/news/${params.newsId}`);
+//   const data = await res.json();
+//   return {
+//     props: {
+//       news: data,
+//     },
+//   };
+// };
